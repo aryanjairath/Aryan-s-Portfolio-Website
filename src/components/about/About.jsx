773f67b9773fb729d14bb2bd05b4e0497bf152c4
Owner <aryanjairath7@gmail.com>
2023-06-13 14:42:18 -0400

    experience/fixed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -101,7 +101,8 @@ const About = () => {
                             <p>I am currenty a second year student at Rutgers</p>
                             <p>University with both Front-End and Back-End experience in programming.</p>
                             <p>I have a 3.92 GPA and have surrounded myself in an environment to further</p> 
-                            <p>my computer science arsenal in any way possible. I currently am involved in the</p>                                <p>Rutgers Security Club.</p>
+                            <p>my computer science arsenal in any way possible. I currently am involved in the</p>                                
+                            <p>Rutgers Security Club.</p>
                             <p>In my free time, I enjoy doing algorithmic style coding problems, making mobile</p>
                             <p>applications particularly on the Android side. I also have a multitude of hobbies: a big Brooklyn Nets and Roger </p>
                             <p>Federer fan and watch sports almost every day and also just hanging out with friends and family whenver I can. </p>                            
