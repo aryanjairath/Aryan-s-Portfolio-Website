af7451f77796533485a053b90ba5b3c9ea7113f6
Owner <aryanjairath7@gmail.com>
2023-11-14 18:38:58 -0500

    added learn more buttons

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -3,7 +3,7 @@ import './work.css'
 import fannie from  '../../assets/fannie.jpg'
 import biomed from  '../../assets/biomed.png'
 import c1 from  '../../assets/capone.png'
-
+import Popup from 'reactjs-popup'
 import genesis from  '../../assets/genesis.jpg'
 import {BsBank, BsHouseDoor} from 'react-icons/bs'
 import {FaRegKeyboard} from 'react-icons/fa'
@@ -12,7 +12,20 @@ import ScrollAnimation from 'react-animate-on-scroll';
 import {BsArrowRight} from 'react-icons/bs'
 import 'animate.css';
 const Work = () => {
-  return (
+    const createParagraphs = (textArray) => {
+        return textArray.map((text, index) => (
+            <p key={index}>{text}</p>
+        ));
+    };
+
+    // Array of strings for the popup content
+    const biomedPop = ["Created and presented engaging, informative lectures about different topics in biology and medicine on a weekly basis throughout the school year. Took into account the interest of club members to develop engaging activities for meetings. Personally invited over 10 guest speakers (including, but not limited to doctors, nurses, and researchers) to provide club members with real-life experiences through a school-wide event, BioMed Career Convention."];
+    const genesisPop = ["• Taught lessons plans with 50+ students pivotal Java concepts such as polymorphism, inheritance, and encapsulation; also assigned a JavaFX game development project",
+    "• Reinforced concepts with limited computer access through exercises such as determine the output and big O",
+    "analysis depending on experience; over 75% of the cohort passed Data Structures certification exam"];
+    const fanniePop =["• Used DBeaver to test PostgreSQL query performance through AWS Redshift saving 10 hours per execution • Worked with Spring Boot and Angular in Social Score Calculator webapp; improved error handling by 20% • AWS environment, implementing SageMaker for advanced data modeling; improved single-family prediction accuracy by 40% for stakeholders, resulting in optimized resource allocation and increased revenue potential. • Created decision trees to compare Dask, Pandas, Spark, and Polars based on cost and performance for utilization • Created a PowerAutomate flow in Microsoft PowerApps to conduct the transfer of files from Sharepoint to S3. • Used tools such as JIRA and performed unit testing with JUnit to validate applied logic and edge cases."]
+    const c1Pop = ["More information coming soon!"]
+    return (
     <section id = 'work'>
         <ScrollAnimation animateIn="animate__animated animate__fadeInRight" duration={3} animateOnce={true}>
             <h5>Work & Leadership</h5>
@@ -31,6 +44,18 @@ const Work = () => {
                             </div>
                         </div>
                     </article>
+                    <Popup trigger={<button className='btn btn-primary'>Learn More</button>} modal nested>
+                {close => (
+                    <div className='modal'>
+                        <div className="pop_up_window animate__animated animate__zoomIn">
+                            {createParagraphs(c1Pop)}
+                        </div>
+                        <button className='btn btn-primary' onClick={() => close()}>
+                            Close
+                        </button>
+                    </div>
+                )}
+            </Popup>
                 </article>
                 <article className="portfolio_item">
                     <article className='experience_details'>
@@ -43,6 +68,18 @@ const Work = () => {
                             </div>
                         </div>
                     </article>
+                    <Popup trigger={<button className='btn btn-primary'>Learn More</button>} modal nested>
+                {close => (
+                    <div className='modal'>
+                        <div className="pop_up_window animate__animated animate__zoomIn">
+                            {createParagraphs(fanniePop)}
+                        </div>
+                        <button className='btn btn-primary' onClick={() => close()}>
+                            Close
+                        </button>
+                    </div>
+                )}
+            </Popup>
                 </article>
                 <article className="portfolio_item">
                     <article className='experience_details'>
@@ -55,18 +92,47 @@ const Work = () => {
                             </div>
                         </div>
                     </article>
+                    <Popup trigger={<button className='btn btn-primary'>Learn More</button>} modal nested>
+                {close => (
+                    <div className='modal'>
+                        <div className="pop_up_window animate__animated animate__zoomIn">
+                            {createParagraphs(genesisPop)}
+                        </div>
+                        <button className='btn btn-primary' onClick={() => close()}>
+                            Close
+                        </button>
+                    </div>
+                )}
+            </Popup>
                 </article>
                 <article className="portfolio_item">
                     <article className='experience_details'>
+                        
                         <BiTestTube className = 'work-option-icon'/>
                         <BsArrowRight/>
+                        
                         <div>
                             <h4>Biomedical Futures: Executive</h4>
                             <div className = 'work-image'>
                                 <img src = {biomed} alt = 'fannie'></img>
+                                
                             </div>
+                            
                         </div>
                     </article>
+                    <Popup trigger={<button className='btn btn-primary'>Learn More</button>} modal nested>
+                {close => (
+                    <div className='modal'>
+                        <div className="pop_up_window animate__animated animate__zoomIn">
+                            {createParagraphs(biomedPop)}
+                        </div>
+                        <button className='btn btn-primary' onClick={() => close()}>
+                            Close
+                        </button>
+                    </div>
+                )}
+            </Popup>
+
                 </article>
         </div>
     </section>
